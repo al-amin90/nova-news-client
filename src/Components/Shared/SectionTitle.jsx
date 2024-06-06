ad6644355206ee44b55c776da91529e96c0323d1
al-amin90 <ijesun30@gmail.com>
2024-06-07 01:53:15 +0600

    footer added

--- src/Components/Shared/SectionTitle.jsx
+++ src/Components/Shared/SectionTitle.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-const SectionTitle = ({ label }) => {
+const SectionTitle = ({ label, text }) => {
   return (
     <div className="flex mb-10 items-end">
-      <h1 className="text-3xl text-white font-nunito font-bold ">{label}</h1>
+      <h1
+        className={`${
+          text ? text : "text-3xl"
+        } text-white font-nunito font-bold `}
+      >
+        {label}
+      </h1>
       <h6 className="bg-[#FF2400] ml-2 mb-2 h-1 -skew-x-[30deg]">
-        <span className="text-transparent">......</span>
+        <span className="text-transparent">..{text ? "" : "...."}</span>
       </h6>
     </div>
   );
